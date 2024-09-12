cd95bf68dad13a691ebb48d66436ee61e6fc5f3f
Bayode Aderinola <bayodesegun@gmail.com>
2024-09-12 21:31:31 +0100

    Create new model ImagePic

--- pilotlog/models/__init__.py
+++ pilotlog/models/__init__.py
@@ -2,10 +2,12 @@ from .pilot_log import PilotLog
 from .aircraft import Aircraft
 from .airfield import Airfield
 from .flight import Flight
+from .image_pic import ImagePic
 
 __all__ = [
     "PilotLog",
     "Aircraft",
     "Airfield",
     "Flight",
+    "ImagePic",
 ]
---
+++ pilotlog/models/image_pic.py
@@ -0,0 +1,39 @@
+from django.db import models
+from .bases import RecordTracking
+
+
+class ImagePic(RecordTracking):
+    img_code = models.CharField(
+        max_length=255,
+        primary_key=True,
+        verbose_name="ImgCode",
+        help_text="Unique identifier for the image"
+    )
+    file_ext = models.CharField(
+        max_length=10,
+        verbose_name="FileExt",
+        help_text="File extension of the image"
+    )
+    file_name = models.CharField(
+        max_length=255,
+        verbose_name="FileName",
+        help_text="Name of the image file"
+    )
+    img_download = models.BooleanField(
+        default=False,
+        verbose_name="Img_Download",
+        help_text="Indicates if the image has been downloaded"
+    )
+    img_upload = models.BooleanField(
+        default=False,
+        verbose_name="Img_Upload",
+        help_text="Indicates if the image has been uploaded"
+    )
+    link_code = models.CharField(
+        max_length=255,
+        verbose_name="LinkCode",
+        help_text="Code linking the image to other records"
+    )
+
+    def __str__(self):
+        return self.img_code
